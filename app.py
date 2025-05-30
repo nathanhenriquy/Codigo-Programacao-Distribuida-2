d2f0474cc4ca84b460c166c5f486dad0ab2b4cdf
Nathan Henrique <nathanlucindosantos@gmail.com>
2025-05-30 19:53:45 -0300

    mudanca main

--- app.py
+++ app.py
@@ -148,10 +148,10 @@ def tarefas_page(usuario_id):
 
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug = True)
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()
+#     app.run(debug = True)
 
-# if __name__ == "__main__":
-# app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
